fe54430a90ba0872422694e0b4040d4d05efa1b4
Aggrey-marvin <zijjaaggreymarvin@gmail.com>
2024-04-10 10:35:07 +0300

    Display all images

---
+++ js/altra.js
@@ -0,0 +1,17 @@
+var imageUrls = [
+    "images/resource/connection.jpeg",
+    "images/resource/connection1.jpeg",
+    "images/resource/connection2.jpeg",
+    "images/resource/connection3.jpeg",
+    "images/resource/connection4.jpeg",
+]
+
+// Function to set a random image source
+function setRandomImage() {
+    var randomNumber = Math.floor(Math.random() * imageUrls.length);
+    var randomImageUrl = imageUrls[randomNumber];
+    document.getElementById("dynamicImage").src = randomImageUrl;
+}
+
+// Call the function to set a random image source initially
+setRandomImage();
